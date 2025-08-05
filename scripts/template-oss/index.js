82476fe87ee6dd9186a93c1d225ec143e3c36604
Karen Li <47998177+karenjli@users.noreply.github.com>
2025-08-05 18:35:12 +0100

    chore: Install template-oss (#9)
    This PR installs template-oss to `api-documentation` repo. It adds the
    following configurations
    1. Customized `ci.yml`, `publish.yml` and `dependabot.yml`
    2. Remove testing for `ci.yml`
    3. Remove `ci-release.yml`, `release-integration.yml`, and `release.yml`
    as these are specific to releasing packages
    4. `ci.yml`: Linting only
    5. `publish.yml`: Build and deploy to Github pages, a draft link will be
    generated if the deployment is from a commit in the PR
    6. `dependabot.yml`: Apply the same customization to this workflow as
    `npm/documentation`
    7. Exclude `/api` from gitignore
    
    Ref: https://github.com/github/npm/issues/14001

---
+++ scripts/template-oss/index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  rootRepo: {
+    add: {
+      '.github/workflows/ci.yml': 'ci-yml.hbs',
+      '.github/workflows/publish.yml': 'publish-yml.hbs',
+      '.github/CODEOWNERS': false,
+      '.github/ISSUE_TEMPLATE/bug.yml': false,
+      '.commitlintrc.js': false,
+      '.github/settings.yml': false,
+      '.github/dependabot.yml': 'dependabot-yml.hbs',
+      '.github/workflows/ci-release.yml': false,
+      '.github/workflows/release-integration.yml': false,
+      '.github/workflows/release.yml': false,
+    },
+  },
+  rootModule: {
+    add: {
+      'package.json': { file: 'package-json.hbs', overwrite: false },
+      '.eslintrc.js': false,
+      'CODE_OF_CONDUCT.md': false,
+      'CONTRIBUTING.md': false,
+    },
+  },
+  workspaceRepo: {
+    add: {},
+  },
+  workspaceModule: {
+    add: {},
+  },
+  ciVersions: 'latest',
+  latestCiVersion: 22,
+  macCI: false,
+  windowsCI: false,
+  lockfile: true,
+  // these need to be allowed since they need to installed explicitly since
+  // the repo uses legacy-peer-deps to avoid gatsby errors
+  allowedPackages: [],
+  requiredPackages: {
+    devDependencies: [],
+  },
+  allowPaths: ['/api/'],
+}
